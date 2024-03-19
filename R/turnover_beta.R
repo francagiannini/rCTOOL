2d5e7a2675f55c9e587d06a0a10e2b066e5244eb
Franca Giannini-Kurina <au710823@uni.au.dk>
2024-03-19 14:49:53 +0100

    rename to check

--- R/turnover_beta.R
+++ R/turnover_beta.R
@@ -7,7 +7,7 @@
 
 # i refers to the time step
 
-turnover <- function(i) {
+turnover_beta <- function(i) {
 
   result_pools <- result_pools[i - 1, ]
   result_pools <- as.data.frame(t(result_pools))
